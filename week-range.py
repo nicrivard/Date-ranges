ed94d77df3d329c348c10ccb0f3136abce294bcd
nicrivard <62037628+nicrivard@users.noreply.github.com>
2020-09-27 20:28:40 +0100

    Added last-week.py

--- week-range.py
+++ week-range.py
@@ -14,3 +14,10 @@ nextStart = today + timedelta(days=7) - timedelta(days=today.weekday())
 nextEnd = nextStart + timedelta(days=6)
 
 print(str(nextStart.strftime("%y%m%d")) + " - " + str(nextEnd.strftime("%y%m%d")))
+
+# last week
+
+lastStart = today - timedelta(days=7) - timedelta(days=today.weekday())
+lastEnd = lastStart + timedelta(days=6)
+
+print(str(lastStart.strftime("%y%m%d")) + " - " + str(lastEnd.strftime("%y%m%d")))
