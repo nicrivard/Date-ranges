d4969142f43278f0498b6952768a921731b79565
nicrivard <62037628+nicrivard@users.noreply.github.com>
2020-10-26 11:40:32 +0000

    Update week-range.py
    Update script with 'days this week' code e.g. [[200409 Monday]], ..., [[200413 Friday]]

--- week-range.py
+++ week-range.py
@@ -21,3 +21,15 @@ lastStart = today - timedelta(days=7) - timedelta(days=today.weekday())
 lastEnd = lastStart + timedelta(days=6)
 
 print(str(lastStart.strftime("%y%m%d")) + " - " + str(lastEnd.strftime("%y%m%d")))
+
+# days this week
+
+Monday = today - timedelta(days=today.weekday())
+Tuesday = Monday + timedelta(days=1)
+Wednesday = Monday + timedelta(days=2)
+Thursday = Monday + timedelta(days=3)
+Friday = Monday + timedelta(days=4)
+Saturday = Monday + timedelta(days=5)
+Sunday = Monday + timedelta(days=6)
+
+print("[[" + str(Monday.strftime("%y%m%d %A")) + "]]" + "\n" + "[[" + Tuesday.strftime("%y%m%d %A") + "]]" + "\n" + "[[" + Wednesday.strftime("%y%m%d %A") + "]]" + "\n"+ "[[" + Thursday.strftime("%y%m%d %A") + "]]" + "\n" + "[[" + Friday.strftime("%y%m%d %A") + "]]")
